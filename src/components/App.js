34442594c705945fc1a5528b2a7b918ed60542eb
cmtp <christian.tola@hotmail.com>
2018-10-29 11:00:39 -0400

    Add Todo sample

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 
-import Home from './Home/Home';
+import Todo from './Todo/Todo';
+
+// import Home from './Home/Home';
 import Header from '../shared/components/layout/Header';
 import Content from '../shared/components/layout/Content';
 import Footer from '../shared/components/layout/Footer';
@@ -13,7 +15,8 @@ class App extends Component {
             <div className="App">
                 <Header title="Welcome to CodeJobs"/>
                 <Content>
-                    <Home/>
+                    <Todo />
+                    {/*<Home/>*/}
                 </Content>
                 <Footer/>
             </div>
