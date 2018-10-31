e5696908a156871ff0eec230d49668b521bc9ea8
cmtp <christian.tola@hotmail.com>
2018-10-31 11:45:55 -0400

    add new component with charts of c3

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
 
-import Notes from './Notes/Notes';
+import Chart from './Chart/Chart';
+// import Notes from './Notes/Notes';
 // import Coins from './Coins/Coins';
 // import Timer from './Pomodoro/Timer';
-// import Todo from './Todo/Todo';
+import Todo from './Todo/Todo';
 // import Home from './Home/Home';
 import Header from '../shared/components/layout/Header';
 import Content from '../shared/components/layout/Content';
@@ -14,34 +15,66 @@ import { notes1, notes2 } from './Notes/data';
 import './App.css';
 
 class App extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
+
+        // this.state = {
+        //     notes: notes1
+        // };
 
         this.state = {
-            notes: notes1
+            chartType: 'line'
         };
+
+        this.columns =
+            [
+                ['BTC', 3000, 6000, 10000, 15000, 13000, 11000],
+                ['ETH', 2000, 3000, 5000, 4000, 3000, 940],
+                ['XRP', 100, 200, 300, 500, 400, 300]
+        ];
     }
 
-    componentDidMount() {
-        setTimeout(() => {
-            this.setState({
-                notes: [...this.state.notes, ...notes2]
-            });
-        }, 10000);
+    setBarChart = () => {
+        this.setState({
+            chartType: 'bar'
+        });
     }
 
-    componentWillUnmount() {
-        console.log('I\'ll be back! ');
-        document.body.style = 'background: black;';
-        document.getElementById('unmountMessage').style.color = 'white';
+    setLineChart = () => {
+        this.setState({
+            chartType: 'line'
+        });
     }
+
+    // componentDidMount() {
+    //     setTimeout(() => {
+    //         this.setState({
+    //             notes: [...this.state.notes, ...notes2]
+    //         });
+    //     }, 10000);
+    // }
+    //
+    // componentWillUnmount() {
+    //     console.log('I\'ll be back! ');
+    //     document.body.style = 'background: black;';
+    //     document.getElementById('unmountMessage').style.color = 'white';
+    // }
 
     render() {
         return (
             <div className="App">
                 <Header title="Welcome to CodeJobs"/>
                 <Content>
-                    <Notes notes={this.state.notes}/>
+                    <Chart columns={this.columns}
+                           chartType={this.state.chartType}
+                    />
+                    <p>
+                        Chart Type
+                        <button onClick={this.setBarChart}>Bar</button>
+                        <button onClick={this.setLineChart}>Line</button>
+                    </p>
+
+                    {/*<Notes notes={this.state.notes}/>*/}
                     {/*<Coins />*/}
                     {/*<Timer />*/}
                     {/*<Todo />*/}
--- src/index.js
+++ src/index.js
@@ -4,19 +4,19 @@ import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-const unmountButton = document.getElementById('unmount');
-
-function unmount() {
-    ReactDOM.unmountComponentAtNode(
-        document.getElementById('root')
-    );
-
-    document.getElementById('unmountMessage').style.display = 'block';
-    unmountButton.remove();
-}
-
-unmountButton.addEventListener('click', unmount);
-document.getElementById('unmountMessage').style.display = 'none';
+// const unmountButton = document.getElementById('unmount');
+//
+// function unmount() {
+//     ReactDOM.unmountComponentAtNode(
+//         document.getElementById('root')
+//     );
+//
+//     document.getElementById('unmountMessage').style.display = 'block';
+//     unmountButton.remove();
+// }
+//
+// unmountButton.addEventListener('click', unmount);
+// document.getElementById('unmountMessage').style.display = 'none';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
