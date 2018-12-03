1a4dc49839b8c53e709fa957e1f2bf11e3b8e941
cmtp <christian.tola@hotmail.com>
2018-12-03 15:25:33 -0400

    add redux and reducers into app

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import { bool } from 'prop-types';
+import { connect } from 'react-redux';
 
 import './Home.css';
 
-const Home = () => (
-  <div className="Home">
-    <h1>Home</h1>
-  </div>
-);
+const Home = props => {
+  const { isMobile } = props;
+  return (
+    <div className="Home">
+      <h1>Home</h1>
 
-export default Home;
+      <p>
+        You are using:
+        <strong>{isMobile ? 'mobile' : 'desktop'}</strong>
+      </p>
+    </div>
+  );
+};
+
+Home.propTypes = {
+  isMobile: bool,
+};
+
+const mapStateToProps = ({device}) => ({
+  isMobile: device.isMobile,
+});
+
+
+export default connect(
+  mapStateToProps,
+  null
+)(Home);
--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import configureStore from './shared/redux/configureStore';
+
 import AppRoutes from './routes';
 
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <Router>
-    <AppRoutes />
-  </Router>,
-  document.getElementById('root')
-);
+const store = configureStore(window.initialState);
+const rootElement = document.getElementById('root');
 
+const renderApp = Component => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <Router>
+        <Component />
+      </Router>
+    </Provider>,
+    rootElement
+  );
+};
+
+renderApp(AppRoutes);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
