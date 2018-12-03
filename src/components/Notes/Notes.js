6e6f288a532ee925f63e8c460ed5a3f339dcfb61
cmtp <christian.tola@hotmail.com>
2018-12-03 13:07:32 -0400

    add Links for navigation into our app

--- src/components/Notes/Notes.js
+++ src/components/Notes/Notes.js
@@ -1,50 +1,60 @@
-/* eslint-disable */
 import React, { Component } from 'react';
-import moment from 'moment';
+import { Link } from 'react-router-dom';
 
 import './Notes.css';
-
-const formatTime = 'YYYY-MM-DD HH:mm:ss';
 
 class Notes extends Component {
   constructor() {
     super();
 
     this.state = {
-      lastUpdate: moment()
-        .format(formatTime)
-        .toString(),
+      notes: [
+        {
+          id: 1,
+          title: 'My note 1',
+        },
+        {
+          id: 2,
+          title: 'My note 2',
+        },
+        {
+          id: 3,
+          title: 'My note 3',
+        },
+      ],
     };
   }
 
-  /* eslint-disable react/prop-types */
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.notes !== this.props.notes) {
-      this.setState({
-        lastUpdate: moment()
-          .format(formatTime)
-          .toString(),
-      });
-    }
-  }
+  renderNotes = notes => (
+    <ul>
+      {notes.map((note, key) => (
+        /* eslint-disable react/no-array-index-key */
+        <li key={key}>
+          <Link to={`/notes/${note.id}`}>{note.title}</Link>
+        </li>
+      ))}
+    </ul>
+  );
 
   render() {
-    const { notes } = this.props;
+    console.log(this.props);
+    /* eslint-disable react/prop-types */
+    const {
+      match: {
+        params: { noteId },
+      },
+    } = this.props;
+    const { notes } = this.state;
+    let selectedNote = false;
+
+    if (noteId > 0) {
+      selectedNote = notes.filter(note => note.id === Number(noteId));
+    }
 
     return (
       <div className="Notes">
-        <h1>Notes:</h1>
-        <ul>
-          {notes.map((note, key) => (
-            <li key={key}>
-              {note.title} - {note.content}
-            </li>
-          ))}
-        </ul>
-
-        <p>
-          Last Update: <strong>{this.state.lastUpdate}</strong>
-        </p>
+        <h1>Notes</h1>
+        {this.renderNotes(selectedNote || notes)}
       </div>
     );
   }
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import App from './components/App';
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Contact from './components/Contact/Contact';
+import Notes from './components/Notes/Notes';
 import Error404 from './components/Error/404';
 
 const AppRoutes = () => (
@@ -13,6 +14,8 @@ const AppRoutes = () => (
       <Route path="/" component={Home} exact />
       <Route path="/about" component={About} exact />
       <Route path="/contact" component={Contact} exact />
+      <Route path="/notes" component={Notes} exact />
+      <Route path="/notes/:noteId" component={Notes} exact />
       <Error404 component={Error404} />
     </Switch>
   </App>
